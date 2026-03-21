644e9507f72ff3b9b8de038a139c4a9b3500e982
Gabriel Lundquist <gabrielmlundquist@yahoo.com>
2026-03-21 17:57:42 -0500

    Experimenting with PHP, some linting

---
+++ PHP tutorial/hello.php
@@ -0,0 +1,14 @@
+Hello World!
+
+<p><em>User agent:</em> <?= $_SERVER['HTTP_USER_AGENT'] ?> </p>
+
+<p><em>Name:</em> <?= $_GET['name'] ?> </p>
+
+<p><em>Age:</em> <?= (int) $_GET['age'] ?> </p>
+
+<?php
+
+// phpinfo();
+// echo '<p><e>User agent:</e> ' . $_SERVER['HTTP_USER_AGENT'] . '</p>';
+
+?>
