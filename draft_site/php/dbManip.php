261be773dbad8954142c05f1c21722a337e23d7b
Gabriel Lundquist <gml7@rice.edu>
2026-04-07 00:08:31 -0500

    Added functions for inserting tag and content warning associations given page record

--- draft_site/php/dbManip.php
+++ draft_site/php/dbManip.php
@@ -1,7 +1,7 @@
 <?php
 namespace Briel {
 
-const SQLLOADNULL = '\N';
+const SQLLOADFILENULL = '\N';
 const SQLNULL = 'NULL';
 
 const FILECOLUMNS = ["fileid",
@@ -61,6 +61,9 @@ function pdoConnect( $dbhost = 'localhost',
     }
 }
 
+/**
+ * 
+ */
 function executeAndFetch($statement, 
                          $executeArr, 
                          $mode = \PDO::FETCH_BOTH) {
@@ -92,7 +95,7 @@ function generateFileRecordInteractive($filePath,
     $fileSizeKB = (int) (filesize($filePath) / 1000);
 
     $ratioStatement = $pdoConn->prepare("SELECT ratioid FROM aspectratio 
-                                            WHERE ratio = :ratioStr");
+                                            WHERE ratio = :ratioStr;");
     $fetchedRow = NULL;
     if (!$ratioStr) {
         $ratioStr = promptInput("Enter aspect ratio (format #:#): > ");
@@ -108,15 +111,15 @@ function generateFileRecordInteractive($filePath,
     }
     $ratioID = $fetchedRow[0];
 
-    return [SQLLOADNULL, 
-            $absPath, 
-            $imgWidth, 
-            $imgHeight, 
-            $alttext, 
-            $fileExt, 
-            $fileSizeKB, 
-            SQLLOADNULL, 
-            $ratioID];
+    return ["fileid"    => SQLNULL, 
+            "location"  => $absPath, 
+            "width"     => $imgWidth, 
+            "height"    => $imgHeight, 
+            "alttext"   => $alttext, 
+            "filetype"  => $fileExt, 
+            "filesize"  => $fileSizeKB, 
+            "pageid"    => SQLNULL, 
+            "ratio"     => $ratioID];
 }
 
 function prepareInsertRecords($pdoConn, 
@@ -141,7 +144,7 @@ function executeInsertRecords($pdoConn,
                               $insertStatement, 
                               $records, 
                               $debugTransaction = true) {
-    $transactSuccess = NULL;
+    $transactSuccess = false;
     try {
         $transactSuccess = $pdoConn->beginTransaction();
     } catch (\PDOException $e) {
@@ -200,16 +203,6 @@ function insertFileRecords($records,
     return $queryResult;
 }
 
-// Do I need this?
-// const ACCEPTEDIMGTYPES = ["jpg",
-//                           "jpeg",
-//                           "png",
-//                           "gif",
-//                           "tif",
-//                           "tiff",
-//                           "ico",
-//                           "bmp"];
-
 function insertFileRecordsFolder($dirPath, $pdoConn) {
 
     $fileInfo = \finfo_open(\FILEINFO_MIME_TYPE);
@@ -248,7 +241,7 @@ function loadFileRecords($dirPath,
 function getFileWidthLocations($pageid, $pdoConn) {
     
     if ($selectFile = $pdoConn->prepare('SELECT width, location FROM file 
-                                         WHERE pageid = ?')
+                                         WHERE pageid = ?;')
             AND $selectFile->execute([$pageid])) {
 
         return $selectFile->fetchAll(\PDO::FETCH_UNIQUE | \PDO::FETCH_ASSOC);
@@ -258,24 +251,28 @@ function getFileWidthLocations($pageid, $pdoConn) {
     }
 }
 
-function is_html_path($str) {
-    return preg_match('(.*/.+\.html)', $str);
+// function is_html_path($str) {
+//     return is_file_path($str) AND preg_match('\.html)', $str);
+// }
+
+function is_file_path($str, $fileExt = ".+") {
+    return preg_match('(.*/.+)', $str) AND preg_match("(.*\.$fileExt)", $str);
 }
 
 function generatePageRecordInteractive($pdoConn) {
-
     echo "Generating new page record...";
     
     // Set title
-    $allTitles = $pdoConn->query("SELECT title FROM page")
+    $allTitles = $pdoConn->query("SELECT title FROM page;")
                           ->fetchAll(\PDO::FETCH_COLUMN, 0);
     $title = promptInput("Enter title: > ");
-    $findFileFromTitle = $pdoConn->prepare("SELECT location FROM page 
-                                            WHERE title = ?");
+    $findPathFromTitle = $pdoConn->prepare("SELECT location FROM page 
+                                            WHERE title = ?;");
     $useAnyway = false;
     while (in_array($title, $allTitles) AND !$useAnyway) {
         echo "Warning: $title already exists in page at "
-             . $findFileFromTitle->execute([$title])->fetch()[0];
+             . $findPathFromTitle->execute([$title])->fetch()[0] 
+             . "\n";
         if (promptInput("Use this title anyway? (y/n) > ") == "y") {
             $useAnyway = true;
         } else $title = promptInput("Enter title: > ");
@@ -287,55 +284,185 @@ function generatePageRecordInteractive($pdoConn) {
                                           . "(doesn't need to exist):"
                                           . "\n> ");
     $location = $promptHTMLPath();
-    while (!is_html_path($location) 
+    while (!is_file_path($location, "html") 
            OR (file_exists($location) AND !$useAnyway)) {
-        if (!is_html_path($location)) {
-            echo "Error: $location is not a path for an HTML file.";
+        if (!is_file_path($location, "html")) {
+            echo "Error: $location is not a path for an HTML file.\n";
             $location = $promptHTMLPath();
         } else if (file_exists($location)) {
             if (promptInput("Warning: $location already exists.\n"
-                            / "Use it? (y/n) > ") == "y") {
+                            . "Use it? (y/n) > ") == "y") {
                 $useAnyway = true;
             } else $location = $promptHTMLPath();
         }
     }
 
+    // Get image description
     $imageDesc = promptInput("Enter comic page description "
                                 . "(or a path to it):\n> ");
     if (is_readable($imageDesc)) {
         $imageDesc = file_get_contents($imageDesc);
     }
 
-    $spreadID = (promptInput("Double spread? (y/n) > ") =="y") ?"":"";
+    // Get spread ID
+    $getSpreadID = $pdoConn->prepare("SELECT spreadid FROM spread
+                                    WHERE spreadtype = ?;");
+    $getSpreadID->execute([(promptInput("Double spread? (y/n) > ") == "y")
+                            ? "double" : "normal"]);
+    $spreadID = $getSpreadID->fetch()[0];
 
-    $fileExt = substr($filePath, strrpos($filePath, ".") + 1);
-
-    $fileSizeKB = (int) (filesize($filePath) / 1000);
-
-    $ratioStatement = $pdoConn->prepare("SELECT ratioid FROM aspectratio 
-                                            WHERE ratio = :ratioStr");
-    $fetchedRow = NULL;
-    if (!$ratioStr) {
-        $ratioStr = promptInput("Enter aspect ratio (format #:#): > ");
+    // Get location of a special style (if present)
+    $styleLocation = promptInput("Enter path to special style (or 'n' if none):\n> ");
+    while (!is_file_path($styleLocation, "css") AND $styleLocation != "n") {
+        echo "Error: $styleLocation not a css file.\n";
+        $styleLocation = promptInput("Enter path to special style (or 'n' if none):\n> ");
     }
-    // if execution is unsuccessful or there is no row in the selection
-    if (!($ratioStatement->execute([":ratioStr" => $ratioStr]))
-            or !($fetchedRow = $ratioStatement->fetch())) {
-        do {
-            $ratioStr = promptInput("$ratioStr not a valid ratio.\n"
-                                    . "Enter aspect ratio (format #:#): > ");
-        } while (!$ratioStatement->execute([":ratioStr" => $ratioStr])
-                 or !($fetchedRow = $ratioStatement->fetch()));
-    }
-    $ratioID = $fetchedRow[0];
+    if ($styleLocation == "n") $styleLocation = SQLNULL;
 
-    return [SQLLOADNULL, 
-            $title, 
-            SQLLOADNULL, // page shouldn't be posted at this point
-            $location, 
-            $imagedesc, 
-            $spreadid, 
-            $stylelocation];
+    return ["pageid"        => SQLNULL, 
+            "title"         => $title, 
+            "location"      => $location, 
+            "imagedesc"     => $imageDesc, 
+            "spreadid"      => $spreadID, 
+            "stylelocation" => $styleLocation];
+}
+
+/**
+ * Assumes $pageRecord is filled out, with an assigned `pageid`.
+ */
+function insertTagAssociationsInteractive($pageRecord, $pdoConn) {
+    if (!$pdoConn->beginTransaction()) {
+        echo "Error: can't begin transaction. Aborting tag assocation.";
+        return false;
+    }
+
+    $existingTags = $pdoConn->query("SELECT name FROM tag;")
+                            ->fetchALL(\PDO::FETCH_COLUMN);
+    $insertNewTag = $pdoConn->prepare("INSERT INTO tag (tagid, name) VALUE (?,?);");
+    $getTagIDFromName = $pdoConn->prepare("SELECT tagid FROM tag WHERE name = ?;");
+
+    $tagListStr = promptInput("Adding tag associations with {$pageRecord['title']}...\n"
+                                . "Type in comma-separated tags. Available tags:\n"
+                                . implode("\t", $existingTags)
+                                . "\n> ");
+    $tagList = array_map(fn($s) => trim($s), explode(",", $tagListStr));
+
+    $tagAssocRecords = [];
+    $finalTagList = [];
+    foreach ($tagList as $tag) {
+
+        if (!preg_match('([\w]+[-\w]*)', $tag)) {
+            echo "Warning: only permitted special characters are - and _\n"
+                . "and - can't start the tag. \nSkipping $tag.\n";
+            continue;
+        } 
+        
+        // Only get below here if the name matches allowed characters.
+        if (!in_array($tag, $existingTags)) {
+            if (promptInput("$tag not an existing tag. Add it? (y/n) > ") == "y") {
+                $insertNewTag->execute([SQLNULL, $tag]);
+            } else {
+                echo "Okay, skipping $tag...\n";
+                continue;
+            }
+        }
+
+        $tagAssocRecords[] = [$getTagIDFromName->execute([$tag])->fetch()[0], 
+                              $pageRecord["pageid"]];
+        $finalTagList[] = $tag;
+    }
+
+    if (queryInsertRecords($pdoConn, 
+                           "tagpage", 
+                           ["tagid", "pageid"], 
+                           $tagAssocRecords)) {
+        if (promptInput("Tags now associated with this page:\n"
+                        . implode("\n", $finalTagList)
+                        . "\nAcceptable? (y/n) > ") == "y") {
+            echo "Sick. Committing...\n";
+            $pdoConn->commit();
+            return $finalTagList;
+        } else {
+            echo "Okay. Rolling back changes...\n";
+            $pdoConn->rollback();
+            return false;
+        }
+    } else {
+        echo "Error: could not insert records. Rolling back and aborting...\n";
+        $pdoConn->rollback();
+        return false;
+    }
+}
+
+/**
+ * Assumes $pageRecord is filled out, with an assigned `pageid`.
+ */
+function insertCWAssociationsInteractive($pageRecord, $pdoConn) {
+    if (!$pdoConn->beginTransaction()) {
+        echo "Error: can't begin transaction. Aborting content warning assocation.";
+        return false;
+    }
+
+    $existingCWs = $pdoConn->query("SELECT name FROM contwarning;")
+                            ->fetchALL(\PDO::FETCH_COLUMN);
+    $insertNewCW = $pdoConn->prepare("INSERT INTO contwarning (contwarningid, name) 
+                                        VALUE (?,?);");
+    $getCWIDFromName = $pdoConn->prepare("SELECT contwarningid FROM contwarning 
+                                            WHERE name = ?;");
+
+    $cwListStr = promptInput("Adding content warning associations with "
+                                . "{$pageRecord['title']}...\n"
+                                . "Type in comma-separated content warnings. Existing warnings:\n"
+                                . implode("\t", $existingCWs)
+                                . "\n> ");
+    $cwList = array_map(fn($s) => trim($s), explode(",", $cwListStr));
+
+    $cwAssocRecords = [];
+    $finalCWList = [];
+    foreach ($cwList as $contWarn) {
+
+        if (!preg_match('([\w]+[-\w]*)', $contWarn)) {
+            echo "Warning: only permitted special characters are - and _\n"
+                . "and - can't start the content warning. \nSkipping $contWarn.\n";
+            continue;
+        } 
+        
+        // Only get below here if the name matches allowed characters.
+        if (!in_array($contWarn, $existingCWs)) {
+            if (promptInput("$contWarn not an existing content warning."
+                            . "Add it? (y/n) > ") == "y") {
+                $insertNewCW->execute([SQLNULL, $contWarn]);
+            } else {
+                echo "Okay, skipping $contWarn...\n";
+                continue;
+            }
+        }
+
+        $cwAssocRecords[] = [$getCWIDFromName->execute([$contWarn])->fetch()[0], 
+                              $pageRecord["pageid"]];
+        $finalCWList[] = $contWarn;
+    }
+
+    if (queryInsertRecords($pdoConn, 
+                           "contwarningpage", 
+                           ["contwarningid", "pageid"], 
+                           $cwAssocRecords)) {
+        if (promptInput("Content warnings now associated with this page:\n"
+                        . implode("\n", $finalCWList)
+                        . "\nAcceptable? (y/n) > ") == "y") {
+            echo "Sick. Committing...\n";
+            $pdoConn->commit();
+            return $finalCWList;
+        } else {
+            echo "Okay. Rolling back changes...\n";
+            $pdoConn->rollback();
+            return false;
+        }
+    } else {
+        echo "Error: could not insert records. Rolling back and aborting...\n";
+        $pdoConn->rollback();
+        return false;
+    }
 }
 
 }
